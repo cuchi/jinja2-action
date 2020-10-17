4e1f15adf976c1e243240f84dc203afae66ab200
Paulo Henrique Cuchi <paulo.cuchi@magrathealabs.com>
2020-10-17 16:52:53 -0300

    refactor: split context class into its own file

--- entrypoint.py
+++ entrypoint.py
@@ -2,38 +2,17 @@
 
 import os
 from jinja2 import Template, StrictUndefined
-from j2cli.context import read_context_data
 
-def guess_format(file_name):
-    _, extension = os.path.splitext(file_name)
-    print(extension)
-    formats = {
-        '.yaml': 'yaml',
-        '.yml': 'yaml',
-        '.json': 'json',
-        '.ini': 'ini',
-        '.env': 'env',
-    }
-    return formats.get(extension, 'env')
-
-variables = {'env': os.environ}
-for variable in os.environ.get('INPUT_VARIABLES', '').split('\n'):
-    clean_variable = bytes(variable.strip(), 'utf-8').decode('unicode_escape')
-    if clean_variable != '':
-        name, value = clean_variable.split('=', 1)
-        variables.update({name: value})
-
-data_file = os.environ.get('INPUT_DATA_FILE')
-if data_file:
-    format = os.environ.get('INPUT_DATA_FORMAT', guess_format(data_file))
-    with open(data_file, 'r') as file:
-        variables.update(read_context_data(format, file, None))
-
+# Montar o objeto do template com as variáveis que eu defini em todos os modos
 with open(os.environ['INPUT_TEMPLATE'], 'r') as file:
     template_kwargs = {}
     if os.environ.get('INPUT_STRICT') == 'true':
         template_kwargs.update({'undefined': StrictUndefined})
     template = Template(str(file.read()), **template_kwargs)
 
+# Aplicar variáveis dentro do template
 with open(os.environ['INPUT_OUTPUT_FILE'], 'w') as file:
     file.write(template.render(**variables) + '\n')
+
+if __name__ == '__main__':
+    main(os.environ)
---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from j2cli.context import read_context_data
+
+
+class Context:
+    def __init__(self, environ):
+        self._variables = {}
+        self._environ = environ
+
+    def load_from_env(self):
+        self._variables.update({'env': self._environ})
+
+    def load_from_input(self):
+        for variable in self._environ.get('INPUT_VARIABLES', '').split('\n'):
+            clean_variable = bytes(variable.strip(), 'utf-8').decode('unicode_escape')
+            if clean_variable != '':
+                name, value = clean_variable.split('=', 1)
+                self._variables.update({name: value})
+
+    def load_from_data_file(self):
+        data_file = self._environ.get('INPUT_DATA_FILE')
+        if data_file:
+            format = self._environ.get('INPUT_DATA_FORMAT', self._guess_format(data_file))
+            with open(data_file, 'r') as file:
+                self._variables.update(read_context_data(format, file, None))
+
+
+    def _guess_format(file_name):
+        _, extension = os.path.splitext(file_name)
+        formats = {
+            '.yaml': 'yaml',
+            '.yml': 'yaml',
+            '.json': 'json',
+            '.ini': 'ini',
+            '.env': 'env',
+        }
+        return formats.get(extension, 'env')
+
+
+def main(environ):
+    context = Context(environ)
+    context.load_from_env()
+    context.load_from_input()
+    context.load_from_data_file()
